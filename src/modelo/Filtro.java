1939bad145c66165d0274dd139161ac5d63cb024
alex <alex@alex-HP-15-Notebook-PC>
2018-06-28 07:24:49 -0600

    ...

---
+++ src/modelo/Filtro.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author alex
+ */
+public class Filtro {
+    
+    private int id;
+    private String codigo;
+    private String marca;
+    private int stock;
+    private boolean existencia;
+    
+    public Filtro(){
+        
+    }
+    
+    public Filtro(String codigo, String marca, int stock, boolean existencia){
+        this.codigo=codigo;
+        this.marca=marca;
+        this.stock=stock;
+        this.existencia=existencia;
+    }
+    
+    public Filtro(String marca, int stock, boolean existencia){
+        this.marca=marca;
+        this.stock=stock;
+        this.existencia=existencia;
+    }
+}
